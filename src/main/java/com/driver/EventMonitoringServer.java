0b44fabcbd7df70bb8e9282ae271a59eb23c0e63
Sunil Reddy Gaddam <gaddamsunilreddy20@gmail.com>
2026-01-16 12:57:44 +0530

    natural events system thread executed

--- src/main/java/com/driver/EventMonitoringServer.java
+++ src/main/java/com/driver/EventMonitoringServer.java
@@ -26,14 +26,41 @@ public class EventMonitoringServer {
 
     private static void startServer() throws InterruptedException {
     	// your code goes here
+        System.out.println("Event monitoring server started. Enter 'shutdown' to stop the server manually.");
+
+        new Thread(() -> {
+            while(true) {
+                String input = getUserInput();
+                if ("shutdown".equalsIgnoreCase(input)) {
+                    System.out.println("Shutting down the server gracefully...");
+                    shutdownLatch.countDown();
+                    break;
+                }
+            }
+        }).start();
+
+        for(int i=0; i<=10; i++){
+            int eventid = i;
+            executorService.submit(()-> processEvent(eventid));
+            Thread.sleep(500);
+        }
+        waitForShutdownSignal();
     }
 
     private static void processEvent(int eventId) {
     	// your code goes here
+        if(eventId == 6 && highMagnitudeEventDetected.compareAndSet(false, true)){
+            System.out.println("High magnitude event detected!");
+            shutdownLatch.countDown();
+            return;
+        }
+
+        System.out.println("Event " + eventId + " processed.");
     }
 
     private static void waitForShutdownSignal() throws InterruptedException {
     	// your code goes here
+        shutdownLatch.await();
     }
 
     private static String getUserInput() {
@@ -43,5 +70,13 @@ public class EventMonitoringServer {
 
     private static void stopServer() {
     	// your code goes here
+        executorService.shutdown();
+        try{
+            if(!executorService.awaitTermination(5, TimeUnit.SECONDS)){
+                executorService.shutdownNow();
+            }
+        }catch(InteruptedException e){
+            executorService.shutdownNow();
+        }
     }
 }
